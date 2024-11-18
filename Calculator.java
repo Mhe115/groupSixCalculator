c7f660a541695d7a119aab7926502ec079f95e5a
juniorokafor <rafaeljrokafor@gmail.com>
2024-11-18 14:43:57 +0000

    main

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
 public class Calculator {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your calculation: ");
+        String calculation = input.nextLine();
 
     }
 }
