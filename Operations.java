759adb1bf260c27415ece923f1f3d4c7971072ef
Mhe115 <conorhealy490@gmail.com>
2024-11-18 15:25:27 +0000

    Trying to divide a Strung into Opperations

--- Operations.java
+++ Operations.java
@@ -6,13 +6,28 @@ public class Operations extends Calculator{
     Operations(){}
 
     protected String Calculate(){
+        String result = "";
+        //while (calculation.length() > 0) {
 
-        while (calculation.length() > 0) {
-            calculation.split("\\+")[0];
+            if (calculation.split("\\*")[0].length() > calculation.split("\\/")[0].length()){
+                //Multiplication
+            }
             
-            
-        }
+            if (calculation.split("\\*")[0].length() < calculation.split("\\/")[0].length()){
+                //Division
+            }
 
-        return calculation;
+            if (calculation.split("\\+")[0].length() > calculation.split("\\-")[0].length()){
+                //Addition
+            }
+
+            else if (calculation.split("\\+")[0].length() < calculation.split("\\-")[0].length()){
+                //Subtraction
+            }
+
+            
+        //}
+
+        return result;
     }
 }
