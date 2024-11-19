f58c24d8bd5a1991de66d588476a38228efc00f4
Blue_Legends <blueronanl@gmail.com>
2024-11-19 11:34:58 +0000

    added open brackets error to calculator

--- Calculator.java
+++ Calculator.java
@@ -6,9 +6,13 @@ public class Calculator {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your calculation: ");
         calculation = input.nextLine();
-        Operations operation = new Operations();
-        operation.Calculate();
+        SyntaxHandler syntax = new SyntaxHandler();
+        if(syntax.verifiyString(calculation)) {
+            Operations operation = new Operations();
+            operation.Calculate();
+        }
         input.close();
+
 
     }
 }
--- SyntaxHandler.java
+++ SyntaxHandler.java
@@ -2,6 +2,7 @@ public class SyntaxHandler {
     public boolean verifiyString(String input)
     {
         boolean valid = true;
+        int open_bracket = 0;
         //remove the
         input = input.replace(" ","");
 
@@ -12,28 +13,44 @@ public class SyntaxHandler {
             first check if every mathmematical operator has a second numberical component
              */
             char test_chr = input.charAt(i);
-            switch (test_chr)
-            {
+            switch (test_chr) {
                 case '+':
-                case '-':
+                    //case '-':
                 case '/':
                 case '*':
                 case '%':
                 case '^':
-                    String value_after_operation = String.valueOf(input.charAt(i+1));
-
                     try {
-                        Double num = Double.parseDouble(value_after_operation);
-                    } catch (NumberFormatException e) {
-                        valid = false;
-                    }
+                        String value_after_operation = String.valueOf(input.charAt(i + 1));
 
+
+                        try {
+                            Double num = Double.parseDouble(value_after_operation);
+                        } catch (NumberFormatException e) {
+                            valid = false;
+                            System.out.println("Syntax Error: please fully complete the operation");
+                        }
+                    } catch (IndexOutOfBoundsException e)
+                    {
+                        valid = false;
+                        System.out.println("Syntax Error: please fully complete the operation");
+                    }
+                    break;
+                case '(':
+                    open_bracket ++;
+                    break;
+                case ')':
+                    open_bracket --;
                     break;
             }
             //System.out.println(input);
         }
 
-
+        if(open_bracket!=0)
+        {
+            valid = false;
+            System.out.println("Syntax Error: please close all brackets in your operation" + open_bracket);
+        }
 
 
         return valid;
